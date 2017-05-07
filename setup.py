1cb521a7d347d97ff30e3437b6f28d54e487a8b8
ran <ran@aroussi.com>
2017-05-07 16:31:02 +0300

    added setup files

---
+++ setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+#
+# Monte Carlo Simulator for Pandas
+# https://github.com/ranaroussi/pandas-montecarlo
+
+"""Monte Carlo Simulator for Pandas"""
+
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='pandas-montecarlo',
+    version="0.0.1",
+    description='Monte Carlo Simulator for Pandas',
+    long_description=long_description,
+    url='https://github.com/ranaroussi/pandas-montecarlo',
+    author='Ran Aroussi',
+    author_email='ran@aroussi.com',
+    license='LGPL',
+    classifiers=[
+        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+        'Development Status :: 3 - Alpha',
+
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'Topic :: Office/Business :: Financial',
+        'Topic :: Office/Business :: Financial :: Investment',
+        'Topic :: Scientific/Engineering :: Interface Engine/Protocol Translator',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    platforms = ['any'],
+    keywords='montecarlo simulator, monte carlo, monte carlo simulation, montecarlo simulation',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests', 'examples']),
+    install_requires=['pandas'],
+    entry_points={
+        'console_scripts': [
+            'sample=sample:main',
+        ],
+    },
+)
